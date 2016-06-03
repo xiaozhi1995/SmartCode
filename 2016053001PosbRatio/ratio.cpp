5ad7d992d5f0ab362436c089694553877ff71183
xiaozhi1995 <1264204146@qq.com>
2016-06-03 20:33:10 +0800

    first

--- 2016053001PosbRatio/ratio.cpp
+++ 2016053001PosbRatio/ratio.cpp
@@ -1,4 +1,5 @@
 //输入n个骰子，求出现所有点数的概率 
+//1.
 #include<iostream>
 #include<math.h>
 using namespace std;
@@ -46,6 +47,66 @@ int main()
 {
 	int n=1;//骰子个数 
 	PrintPossiblity(n); 
+	int n1=2;//骰子个数 
+	PrintPossiblity(n1); 
+	return 0;
+}
+//2.
+#include<stdlib.h>
+#include<math.h>
+#include<iostream>
+using namespace std;
+int g_val=6;//骰子点数 
+void PrintPossiblity(int n)
+{
+	if(n<1)
+	{
+		return;
+	}
+	int* pos[2];
+	pos[0]=new int[g_val*n+1];
+	pos[1]=new int[g_val*n+1];
+	for(int i=0;i<g_val*n+1;++i)
+	{
+		pos[0][i]=0;
+		pos[1][i]=0;
+	}
+	int flag=0;
+	for(int i=1;i<=g_val;++i)
+	{
+		pos[flag][i]=1;
+	}
+	for(int k=2;k<=n;++k)
+	{
+		for(int i=0;i<k;++i)
+		{
+			pos[1-flag][i]=0;
+		}
+		for(int i=k;i<=g_val*k;++i)
+		{
+			pos[1-flag][i]=0;
+			for(int j=1;j<=i&&j<=g_val;++j)
+			{
+				pos[1-flag][i]+=pos[flag][i-j]; 
+			}
+		}
+		flag=1-flag;
+	}
+	int total=pow((double)g_val,n);
+	double ratio=0.0;
+	for(int i=n;i<=g_val*n;++i)
+	{
+		ratio=(double)pos[flag][i]/total;
+		cout<<i<<"ratio"<<ratio<<"  ";
+	}
+	delete[] pos[0];
+	delete[] pos[1];
+}
+int main()
+{
+	int n=1;//骰子个数 
+	PrintPossiblity(n); 
+	cout<<endl;
 	int n1=2;//骰子个数 
 	PrintPossiblity(n1); 
 	return 0;
